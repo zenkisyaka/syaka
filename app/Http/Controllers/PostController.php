ffdd4a402144f139df11f45d889a1607d37bbad9
zenki0815 <zrs0735zrs@icloud.com>
2024-09-25 22:19:55 +0900

    ”レバテックカレッジ”

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,4 +11,10 @@ class PostController extends Controller
     {
         return view('posts.index')->with(['posts' => $post->getPaginateBylimit(3)]);
     }
+
+    public function show(Post $post)
+{
+    return view('posts.show')->with(['post' => $post]);
+ //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+}
 }
