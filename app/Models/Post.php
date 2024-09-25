deaa47f7e5f58ae11a7fc234485657bb9cc92f64
zenki0815 <zrs0735zrs@icloud.com>
2024-09-25 20:57:01 +0900

    ればたっくカレッジ7－2

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+     
+    public function getByLimit(int $limit_count = 10)
+    {
+         return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
+    }
+
+    public function getPaginateByLimit(int $limit_count = 10)
+    {
+         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
 }
