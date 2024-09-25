40bba564e459ed1194ddc0370a461139f45c13ca
zenki0815 <zrs0735zrs@icloud.com>
2024-09-25 23:15:54 +0900

    レバテックカレッジ7－4”
    ds
    
    sdfsd
    git status
    
    sfasfsa

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Http\Request\PostRequest;
 use App\Models\Post;
 
 class PostController extends Controller
@@ -13,8 +13,18 @@ class PostController extends Controller
     }
 
     public function show(Post $post)
-{
+    {
     return view('posts.show')->with(['post' => $post]);
  //'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
-}
+    }
+     public function create()
+    {
+         return view('posts.create');
+    }
+    public function store(Post $post, PostRequest $request, )
+    {
+    $input = $request['post'];
+    $post->fill($input)->save();
+    return redirect('/posts/' . $post->id);
+    }
 }
