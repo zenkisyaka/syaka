1d0d2afadf3908f41a0428d1e6f1c2bf959bb976
zenki0815 <zrs0735zrs@icloud.com>
2024-09-26 00:06:21 +0900

    レバテックカレッジ

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,10 +21,21 @@ class PostController extends Controller
     {
          return view('posts.create');
     }
-    public function store(Post $post, PostRequest $request, )
+    public function store(PostRequest $request, Post $post)
     {
     $input = $request['post'];
     $post->fill($input)->save();
+    return redirect('/posts/' . $post->id);
+    }
+    public function edit(Post $post)
+    {
+    return view('posts.edit')->with(['post' => $post]);
+    }
+    public function update(PostRequest $request, Post $post)
+    {
+    $input_post = $request['post'];
+    $post->fill($input_post)->save();
+
     return redirect('/posts/' . $post->id);
     }
 }
