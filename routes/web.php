95534281650958386651fccd7e645cb3b89b8377
zenki0815 <zrs0735zrs@icloud.com>
2024-09-26 11:31:08 +0900

    レバテックカレッジ7－6

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ Route::get('/posts/{post}', [PostController::class ,'show']);
 Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
 Route::put('/posts/{post}', [PostController::class, 'update']);
+Route::delete('/posts/{post}', [PostController::class,'delete']);
 // '/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
